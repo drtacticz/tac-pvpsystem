18f5e21fd205c35a252966deae42daf2ff8ab5e6
T A C <drtacticzz@gmail.com>
2026-03-20 23:33:16 +0000

    Delete client directory

--- client/antivdm.lua
+++
@@ -1,6 +0,0 @@
-Citizen.CreateThread(function()
-    while true do
-        N_0x4757f00bc6323cfe(-1553120962, 0.0) --- 0.0 Is Damage Modifer/// Turn to 0.5 for 1/2 // Half Damage or `1.0` for full damage!
-        Wait(0) ----every tick
-    end
-end)
